3dcf22b9b484264fe46fe97ca994fc288cb7c6a3
EthChil <ethanchilderhose@gmail.com>
2020-06-09 14:44:25 -0400

    firmware

---
+++ Firmware/Testing/BlinkTesting/cmake-build-debug-mingw/CMakeFiles/BlinkTesting.elf.dir/DependInfo.cmake
@@ -0,0 +1,74 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/Startup/startup_stm32f334r8tx.s" "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/cmake-build-debug-mingw/CMakeFiles/BlinkTesting.elf.dir/Startup/startup_stm32f334r8tx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F334x8"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/CMSIS/Include"
+  "../Drivers/STM32F3xx_HAL_Driver/Inc"
+  "../Drivers/STM32F3xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F3xx/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal.c" "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/cmake-build-debug-mingw/CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal.c.obj"
+  "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_cortex.c" "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/cmake-build-debug-mingw/CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_cortex.c.obj"
+  "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_dma.c" "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/cmake-build-debug-mingw/CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_dma.c.obj"
+  "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash.c" "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/cmake-build-debug-mingw/CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash.c.obj"
+  "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash_ex.c" "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/cmake-build-debug-mingw/CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash_ex.c.obj"
+  "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_gpio.c" "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/cmake-build-debug-mingw/CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_gpio.c.obj"
+  "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c.c" "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/cmake-build-debug-mingw/CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c.c.obj"
+  "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c_ex.c" "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/cmake-build-debug-mingw/CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c_ex.c.obj"
+  "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr.c" "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/cmake-build-debug-mingw/CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr.c.obj"
+  "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr_ex.c" "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/cmake-build-debug-mingw/CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr_ex.c.obj"
+  "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc.c" "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/cmake-build-debug-mingw/CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc.c.obj"
+  "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc_ex.c" "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/cmake-build-debug-mingw/CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc_ex.c.obj"
+  "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim.c" "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/cmake-build-debug-mingw/CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim.c.obj"
+  "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim_ex.c" "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/cmake-build-debug-mingw/CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim_ex.c.obj"
+  "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart.c" "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/cmake-build-debug-mingw/CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart.c.obj"
+  "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart_ex.c" "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/cmake-build-debug-mingw/CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart_ex.c.obj"
+  "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/Src/main.c" "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/cmake-build-debug-mingw/CMakeFiles/BlinkTesting.elf.dir/Src/main.c.obj"
+  "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/Src/stm32f3xx_hal_msp.c" "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/cmake-build-debug-mingw/CMakeFiles/BlinkTesting.elf.dir/Src/stm32f3xx_hal_msp.c.obj"
+  "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/Src/stm32f3xx_it.c" "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/cmake-build-debug-mingw/CMakeFiles/BlinkTesting.elf.dir/Src/stm32f3xx_it.c.obj"
+  "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/Src/syscalls.c" "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/cmake-build-debug-mingw/CMakeFiles/BlinkTesting.elf.dir/Src/syscalls.c.obj"
+  "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/Src/sysmem.c" "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/cmake-build-debug-mingw/CMakeFiles/BlinkTesting.elf.dir/Src/sysmem.c.obj"
+  "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/Src/system_stm32f3xx.c" "C:/Users/ethan/Documents/GitHub/Nimbus/Firmware/Testing/BlinkTesting/cmake-build-debug-mingw/CMakeFiles/BlinkTesting.elf.dir/Src/system_stm32f3xx.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "DEBUG"
+  "STM32F334x8"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/CMSIS/Include"
+  "../Drivers/STM32F3xx_HAL_Driver/Inc"
+  "../Drivers/STM32F3xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F3xx/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Firmware/Testing/BlinkTesting/cmake-build-debug/CMakeFiles/BlinkTesting.elf.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "BlinkTesting.elf"
+  "BlinkTesting.elf.manifest"
+  "BlinkTesting.elf.pdb"
+  "CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal.c.obj"
+  "CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_cortex.c.obj"
+  "CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_dma.c.obj"
+  "CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash.c.obj"
+  "CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash_ex.c.obj"
+  "CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_gpio.c.obj"
+  "CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c.c.obj"
+  "CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c_ex.c.obj"
+  "CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr.c.obj"
+  "CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr_ex.c.obj"
+  "CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc.c.obj"
+  "CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim.c.obj"
+  "CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim_ex.c.obj"
+  "CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart.c.obj"
+  "CMakeFiles/BlinkTesting.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart_ex.c.obj"
+  "CMakeFiles/BlinkTesting.elf.dir/Src/main.c.obj"
+  "CMakeFiles/BlinkTesting.elf.dir/Src/stm32f3xx_hal_msp.c.obj"
+  "CMakeFiles/BlinkTesting.elf.dir/Src/stm32f3xx_it.c.obj"
+  "CMakeFiles/BlinkTesting.elf.dir/Src/syscalls.c.obj"
+  "CMakeFiles/BlinkTesting.elf.dir/Src/sysmem.c.obj"
+  "CMakeFiles/BlinkTesting.elf.dir/Src/system_stm32f3xx.c.obj"
+  "CMakeFiles/BlinkTesting.elf.dir/Startup/startup_stm32f334r8tx.s.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/BlinkTesting.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
